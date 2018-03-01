dcc688f2e79864bf0310cdb4d11064f997495f8b
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-01 12:30:44 +0200

    :wrench: Pretty JSON output

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import * as mount from 'koa-mount';
 import * as Router from 'koa-router';
 import * as bodyparser from 'koa-bodyparser';
 import * as serve from 'koa-static';
-//import { Slack } from './slack';
+import * as koaJson from 'koa-json';
+import { Slack } from './slack';
 import { getActivities } from './strava';
 
 const app = new Koa();
 const router = new Router();
-//const slack = new Slack();
+const slack = new Slack();
 
 app.use(bodyparser());
+app.use(koaJson());
 
 if (process.env.LOG_DESKTOP_HTTP || true) app.use(logger());
 
@@ -23,7 +25,7 @@ router.get('/activities', async (ctx: Router.IRouterContext, next: () => Promise
   ctx.body = await getActivities(20);
 });
 
-//router.post('/webhook', slack.handleSlackIncoming);
+router.post('/webhook', slack.handleSlackIncoming);
 
 app.use(router.routes() as any);
 app.use(serve('static'));
